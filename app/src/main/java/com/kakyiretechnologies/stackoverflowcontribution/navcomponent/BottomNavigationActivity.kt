e84f92b6f796727af02aabcdffa15c2ffaa3d4ce
Kakyire <47930771+Kakyire@users.noreply.github.com>
2022-02-23 13:30:59 +0000

    Contributed to BottomNavigation problem

---
+++ app/src/main/java/com/kakyiretechnologies/stackoverflowcontribution/navcomponent/BottomNavigationActivity.kt
@@ -0,0 +1,10 @@
+package com.kakyiretechnologies.stackoverflowcontribution.navcomponent
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class BottomNavigationActivity:AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
---
+++ app/src/main/java/com/kakyiretechnologies/stackoverflowcontribution/navcomponent/QuoteFragment.kt
@@ -0,0 +1,7 @@
+package com.kakyiretechnologies.stackoverflowcontribution.navcomponent
+
+import androidx.fragment.app.Fragment
+import com.kakyiretechnologies.stackoverflowcontribution.R
+
+class QuoteFragment:Fragment(R.layout.fragment_quote) {
+}
